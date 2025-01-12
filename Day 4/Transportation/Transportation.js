0f445a50c41024712a9e9f81a98d33b5d47af3cc
Rahma Mostafa <rahmamostafa282@gmail.com>
2025-01-12 21:33:08 +0200

    Day4

--- Day 4/Transportation/Transportation.js
+++ Day 4/Transportation/Transportation.js
@@ -11,23 +11,18 @@ You're tasked with modeling vehicles and cars in a transportation app:
     b- Write a function that checks whether an object is an instance of Car using two different ways
 
 */
+let instanceCounter =0;
 function Vehicle(type, speed) { //function constructor
-    //check if Vehicle.instanceCounter is null or undefined so i will intilize it only the first time the Vehicle constructor is called so i ensure that the counter starts from 0 when no instances have been created yet  -- Vehicle.instanceCounter is the name of the property attached to the Vehicle constructor functio
-    if (!Vehicle.instanceCounter) {  
-        Vehicle.instanceCounter = 0;                              
-    }
+    
+    instanceCounter++;
 
-    if (Vehicle.instanceCounter >= 50) {
+    if (instanceCounter >= 50) {
         throw new Error('Vehicle instance limit reached');
     }
-
-    Vehicle.instanceCounter++;
-
     this.speed = speed;
     this.type = type;
     
     this.start = function() {               // create a method for each instance of the Vehicle object -- this ensure that each instance of Vehicle (or its subclasses) will have its own start method. Each object can call start() on its own.
-
         console.log("Vehicle starting");
     };
     this.stop = function() {
@@ -72,7 +67,7 @@ try {
     car1.start();
     car1.stop();
     car1.drive();
-    console.log('Vehicle instance count:', Vehicle.instanceCounter);  //1
+    console.log('Vehicle instance count:', instanceCounter);  //1
 
 
     const vehicle1 = new Vehicle('Truck', 80);
@@ -82,12 +77,12 @@ try {
     vehicle1.stop();
 
     //vehicle1.drive();// throw not a function in veihcle
-    console.log('Vehicle instance count:', Vehicle.instanceCounter);
+    console.log('Vehicle instance count:', instanceCounter);
     
     for (let i = 0; i < 50; i++) {  //create 50 instance of veihcle
         new Vehicle('Car' + i, 100);
     }
-    console.log('Vehicle instance count: ', Vehicle.instanceCounter);  //52 throw limted
+    console.log('Vehicle instance count: ', instanceCounter);  //52 throw limted
 } catch (error) {
     console.error(error.message);
 }
